e933d337f79bb6a4af713f9c3cd0cabe4a0e74fc
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2021-04-12 17:51:30 -0400

    update webproject

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,11 +13,13 @@ import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  containerStyle?: React.CSSProperties;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FunctionComponent<InputProps> = ({
   name,
+  containerStyle = {},
   icon: Icon,
   ...props
 }) => {
